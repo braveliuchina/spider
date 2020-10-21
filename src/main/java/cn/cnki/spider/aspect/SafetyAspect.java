2f3ae12f98595cfa5c329054d1f7fdd8f69daba9
braveliu <braveliuchina@gmail.com>
2020-10-21 09:01:19 +0800

    [add]maven引入依赖

--- src/main/java/cn/cnki/spider/aspect/SafetyAspect.java
+++ src/main/java/cn/cnki/spider/aspect/SafetyAspect.java
@@ -36,8 +36,6 @@ public class SafetyAspect {
     /**
      * Pointcut 切入点
      * 匹配
-     * cn.huanzi.qch.baseadmin.sys.*.controller、
-     * cn.huanzi.qch.baseadmin.*.controller包下面的所有方法
      */
     @Pointcut(value = "execution(public * cn.cnki.spider.sys.*.controller.*.*(..)) || " +
             "execution(public * cn.cnki.spider.sys.*.controller.*.*(..))")
